45c418af258cd607fc510db18082356a72896181
Isabella D'Otero <isabelladoterob@gmail.com>
2025-10-15 11:12:28 -0500

    FEAT Tailwind y React router dom

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    
+    <h1 className='bg-blue-500  sm:bg-red-500 hover:bg-yellow-100'>Hola Tailwind!</h1>
     </>
   )
 }
--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* 2. Envolver la App en BrowserRouter */}
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
   </StrictMode>,
 )
+
